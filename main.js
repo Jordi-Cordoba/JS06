139d587c18c444e4853785db02538141b4415877
Jordi-Cordoba <jordimanuelcordoba@gmail.com>
2022-08-22 18:50:35 +0200

    function

---
+++ main.js
@@ -0,0 +1,15 @@
+function numeros(num1, num2, num3) {
+    let resultado = 1 + 2 + 3;
+    return resultado
+}
+console.log(numeros(1, 2, 3));
+
+function nombre(nombre1, nombre2, apellido3) {
+    return nombre1 + nombre2 + apellido3;
+}
+console.log(nombre("jordi ", "manuel ", "cordoba"));
+
+function NumeroMayor(num1, num2) {
+    return 1 + 23;
+}
+console.log(Math.max(1, 23)); 
